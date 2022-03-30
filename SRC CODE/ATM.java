80c3b9df969a9ef89f04f036a0999a07d6076d8e
T.Divya <87364956+DivyaTheetla@users.noreply.github.com>
2022-03-30 19:44:01 +0530

    CORE JAVA ATM PROJECT

---
+++ SRC CODE/ATM.java
@@ -0,0 +1,41 @@
+package mypackage;
+
+public class ATM {
+    private double balance=5000;
+    private double depositAmount;
+    private double withdrawAmount;
+    
+    
+
+    
+   // public ATM(){
+
+    //}
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(double depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
+
+    public void setWithdrawAmount(double withdrawAmount) {
+        this.withdrawAmount = withdrawAmount;
+    }
+    
+    	
+    }
+
